31e67c3d9ef3c039df872e7b31385cdecc855ed9
DaianeBS <97747870+Daianebs@users.noreply.github.com>
2023-10-06 23:54:37 -0300

    matricula-de-alunos

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// [Template original no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// [Template Daiane no Kotlin Playground] (https://pl.kotl.in/X-VlBvB_p)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: String = "1 hr", val nivel: Nivel = Nivel.BASICO)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val inscritosLista = mutableMapOf<ConteudoEducacional, MutableList<Usuario>>()
+
+    fun matricular(usuario: Usuario, conteudo: ConteudoEducacional) {
+        val inscritos = inscritosLista.getOrPut(conteudo) { mutableListOf() }
+        //verifica se já tem uma lista de conteudo criada para adicionar o usuario
+        inscritos.add(usuario)
+    }
+
+    fun listarInscritosPorConteudo() {
+        for ((conteudo, inscritos) in inscritosLista) {
+            println("________________________________________________")
+            println("->Conteúdo: ${conteudo.nome}")
+            println("${conteudo.nivel}      ${conteudo.duracao} ")
+            println("------------------------------------------------")
+            println("Inscritos:")
+            for (usuario in inscritos) {
+                println("  - ${usuario.nome};")
+            }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin")
+    val conteudo2 = ConteudoEducacional("O Poder das Funções em Kotlin", "2 hrs")
+    val conteudo3 = ConteudoEducacional("Tratamento das Excessões em Kotlin", "2 hrs", Nivel.INTERMEDIARIO)
+
+    val formacao = Formacao("Conhecendo a Linguagem de Programação Kotlin", listOf(conteudo1, conteudo2, conteudo2))
+
+    val aluno1 = Usuario("Alice")
+    val aluno2 = Usuario("Bruna")
+    val aluno3 = Usuario("Carina")
+
+    formacao.matricular(aluno1, conteudo1)
+    formacao.matricular(aluno2, conteudo2)
+    formacao.matricular(aluno3, conteudo1)
+    formacao.matricular(aluno3, conteudo3)
+
+    formacao.listarInscritosPorConteudo()
 }
